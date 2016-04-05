10fbc7d5036cf7c41522be83efd39e1c284f6b04
José Reato Neto <josereato@outlook.com>
2016-04-04 23:02:49 -0300

    Teste Solution Sixsigma

--- View/App_Start/BundleConfig.cs
+++ View/App_Start/BundleConfig.cs
@@ -9,23 +9,21 @@ namespace View
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/AdminLTE-2.3.0/plugins/jQuery/jQuery-2.1.4.min.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
+           
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/bootstrap.js" ));
+
+            bundles.Add(new ScriptBundle("~/bundles/AdminLTE-2.3.0/js").Include(
+                      "~/Scripts/AdminLTE-2.3.0/app.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap/bootstrap.css" ));
+
+            bundles.Add(new StyleBundle("~/AdminLTE-2.3.0/css").Include(
+                      "~/Content/AdminLTE-2.3.0/AdminLTE.min.css",
+                      "~/Content/AdminLTE-2.3.0/skins/skin-blue.min.css"));
         }
     }
 }
