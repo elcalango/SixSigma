3ac2a15b807576329ff0d021e96ec862504f4590
José Reato Neto <josereato@outlook.com>
2016-04-27 07:00:07 -0300

    sdf

--- View/Controllers/RegionController.cs
+++ View/Controllers/RegionController.cs
@@ -9,6 +9,8 @@ using System.Web.Mvc;
 using View.Models;
 using PagedList;
 using View.Helpers;
+using AutoMapper.QueryableExtensions;
+using DelegateDecompiler;
 
 namespace View.Controllers
 {
@@ -20,26 +22,24 @@ namespace View.Controllers
         {
             _regionService = new RegionService();
 
+            //var regions = _regionService.GetRegions().ToPagedList<Region>(page, 2);
+
             var config = new MapperConfiguration(cfg =>
             {
-                cfg.CreateMap<Region, RegionViewModel>();
+                cfg.CreateMap<PagedList<Region>, PagedList<RegionViewModel>>()
+                    .ConvertUsing<PagedListConverter>();
             });
 
+
+            config.CreateMapper(new PagedListTypeConverter<Region, RegionViewModel>)
             IMapper mapper = config.CreateMapper();
 
-            var regions = _regionService.GetRegions().ToPagedList(page, 2);
 
-            
+            var viewModels = Mapper.Map<PagedList<Region>, PagedList<RegionViewModel>>(regions);
 
-            var model = mapper.Map<IPagedList<Region>, IPagedList<RegionViewModel>>(regions);
 
-            //IMapper mapper = Mapper.CreateMap<Region, RegionViewModel>();
-            //Mapper
-            //   .CreateMap<PagedList<Region>, PagedList<RegionViewModel>>()
-            //   .ConvertUsing<PagedListConverter>();
 
-            //model = mapper.ToMappedPagedList<Region, RegionViewModel>(regions);
-            return View(model);
+            return View(viewModels);
         }
 
         public ActionResult Index()
@@ -61,27 +61,63 @@ namespace View.Controllers
             return View(model);
         }
 
-        //public static IPagedList<TDestination> ToMappedPagedList<TSource, TDestination>(this IPagedList<TSource> list)
-        //{
-        //    IEnumerable<TDestination> sourceList = Mapper.Map<IEnumerable<TSource>, IEnumerable<TDestination>>(list);
-        //    IPagedList<TDestination> pagedResult = new StaticPagedList<TDestination>(sourceList, list.GetMetaData());
-        //    return pagedResult;
 
-        //}
+    }
 
-    
-        /// <summary>       
-        /// Search LossReserve by filter as parameter - History       
-        /// </summary>       
-        /// <param name="filter">filter parameters</param>        
-        /// <returns>A <see cref="ApplicationUserSearchResult"/></returns>        
-        //public IPagedList<LossReserveResult> GetLossReserveHistoryByCoverageId(LossReserveHistory filter)
-        //{
-        //    var lossReserveFilter = filter.Map(new BusinessModel.EntityCustom.LossReserveFilter());
-        //    var lossReserveResult = this.lossReserveBusiness.GetLossReserveHistoryByCoverageId(lossReserveFilter);
-        //    var lossReserves = lossReserveResult.Result.Map(new List<LossReserveResult>());
-        //    var pagedList = new StaticPagedList<LossReserveResult>(lossReserves, lossReserveResult.Offset, lossReserveResult.Limit, lossReserveResult.Count);
-        //    return pagedList;
-        //}
+    public class PagedViewModel<T>
+    {
+        public int FirstItemOnPage { get; set; }
+        public bool HasNextPage { get; set; }
+        public bool HasPreviousPage { get; set; }
+        public bool IsFirstPage { get; set; }
+        public bool IsLastPage { get; set; }
+        public int LastItemOnPage { get; set; }
+        public int PageCount { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public int TotalItemCount { get; set; }
+        public IEnumerable<T> rows { get; set; }
+    }
+
+    public class PagedListTypeConverter<T> : ITypeConverter<IPagedList<T>, PagedViewModel<T>>
+    {
+        public PagedViewModel<T> Convert(ResolutionContext context)
+        {
+            var source = (IPagedList<T>)context.SourceValue;
+            return new PagedViewModel<T>()
+            {
+                FirstItemOnPage = source.FirstItemOnPage,
+                HasNextPage = source.HasNextPage,
+                HasPreviousPage = source.HasPreviousPage,
+                IsFirstPage = source.IsFirstPage,
+                IsLastPage = source.IsLastPage,
+                LastItemOnPage = source.LastItemOnPage,
+                PageCount = source.PageCount,
+                PageNumber = source.PageNumber,
+                PageSize = source.PageSize,
+                TotalItemCount = source.TotalItemCount,
+                rows = source
+            };
+        }
+    }
+
+    public class PagedListConverter : ITypeConverter<PagedList<Region>, PagedList<RegionViewModel>>
+    {
+        public PagedList<RegionViewModel> Convert(ResolutionContext context)
+        {
+            var model = (PagedList<Region>)context.SourceValue;
+            var vm = model.Select(m => Mapper.Map<Region, RegionViewModel>(m)).ToList();
+
+            return new PagedList<RegionViewModel>(vm, model.PageNumber, model.PageSize);
+        }
+    }
+
+    public static class MapperExtensions
+    {
+        public static IPagedList<TDestination> ProjectToPagedList<TDestination>(this IQueryable queryable, MapperConfiguration config,
+            int pageNumber, int pageSize)
+        {
+            return queryable.ProjectTo<TDestination>(config).Decompile().ToPagedList(pageNumber, pageSize);
+        }
     }
 }
